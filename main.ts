536436fecfd7614c3dabf5570dfe8f5685465374
lakyung <90668130+lakyung@users.noreply.github.com>
2023-08-09 21:53:38 +0900

    MakeCode 프로젝트용 기초 파일

---
+++ main.ts
@@ -0,0 +1,59 @@
+let 주사위 = 0
+input.onGesture(Gesture.Shake, function () {
+    주사위 = randint(1, 6)
+    if (주사위 == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(1000)
+    } else if (주사위 == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # . # .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(1000)
+    } else if (주사위 == 3) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . . # . .
+            . . . # .
+            . . . . .
+            `)
+        basic.pause(1000)
+    } else if (주사위 == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+        basic.pause(1000)
+    } else if (주사위 == 5) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+        basic.pause(1000)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . # . # .
+            . # . # .
+            . . . . .
+            `)
+        basic.pause(1000)
+    }
+})
